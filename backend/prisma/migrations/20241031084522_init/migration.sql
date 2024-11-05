5d85d9153056aa7814b485317a9d70ca8ec3512b
vinnie0987 <106440414+vinnie0987@users.noreply.github.com>
2024-11-05 15:50:52 +0300

    first commit-database prisma

---
+++ backend/prisma/migrations/20241031084522_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "user_id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
